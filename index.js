3666437e4c75925084aab110c8a7f9089d86bee9
BullerWilliam <166019658+BullerWilliam@users.noreply.github.com>
2026-01-30 21:47:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 
+console.log(process.env.IMAGGA_KEY, process.env.IMAGGA_SECRET);
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
